befc2873892231af2748fd9a8002b751d662f4db
MoncozGC <jadepengc@163.com>
2023-01-06 09:56:13 +0800

    feat: 数据库工具 - 使用配置文件的形式读取mysql和ck数据库

--- utils/databas_dev.py
+++ utils/databas_dev.py
@@ -6,18 +6,22 @@
 
 if __name__ == '__main__':
     # 加载配置文件
-    from utils.connect_databases import load_config
-
-    appRootDir, config = load_config('Python-Employ', ' ')
-    print(appRootDir)
-
-    # 初始化数据库
-    from utils.connect_databases import load_database_config
-
-    load_database_config(config)
+    # from utils.connect_databases import load_config
+    #
+    # appRootDir, config = load_config('Python-Learn', ' ')
+    #
+    # # 初始化数据库
+    # from utils.connect_databases import load_database_config
+    #
+    # load_database_config(config)
     from utils.connect_databases import getDatabaseOperation
 
-    sql1 = """ SELECT * FROM org_customer_nmpa LIMIT 1"""
+    sql1 = """ SELECT * FROM weather LIMIT 1"""
 
-    query = getDatabaseOperation("bhds_base").query(sql1)
+    query = getDatabaseOperation("world").query(sql1)
     print(query)
+
+    sql2 = """ SELECT * FROM ads_id_odr_details_out_1d LIMIT 1"""
+
+    query2 = getDatabaseOperation("bhds_bi").query(sql2)
+    print(query2)
