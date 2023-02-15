216849a118646e0f42cd4598b16f68813828041c
MoncozGC <jadepengc@163.com>
2023-02-15 16:34:43 +0800

    fix: 数据库调试测试

--- utils/databas_dev.py
+++ utils/databas_dev.py
@@ -6,19 +6,19 @@
 
 if __name__ == '__main__':
     # 加载配置文件
-    # from utils.connect_databases import load_config
-    #
-    # appRootDir, config = load_config('Python-Learn', ' ')
-    #
-    # # 初始化数据库
-    # from utils.connect_databases import load_database_config
-    #
-    # load_database_config(config)
+    from utils.connect_databases import load_config
+
+    appRootDir, config = load_config('Python-Learn', ' ')
+
+    # 初始化数据库
+    from utils.connect_databases import load_database_config
+
+    load_database_config(config)
     from utils.connect_databases import getDatabaseOperation
 
-    sql1 = """ SELECT * FROM weather LIMIT 1"""
+    sql1 = """ SELECT * FROM cms_subject LIMIT 1"""
 
-    query = getDatabaseOperation("world").query(sql1)
+    query = getDatabaseOperation("dell_dev").query(sql1)
     print(query)
 
     sql2 = """ SELECT * FROM ads_id_odr_details_out_1d LIMIT 1"""
