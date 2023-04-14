7eae41880f193fcca9e0d7ad8a095564374df208
MoncozGC <jadepengc@163.com>
2023-04-14 10:57:42 +0800

    feat: reptile - 添加注释

--- project/reptile/learning/baidu_fanyi.py
+++ project/reptile/learning/baidu_fanyi.py
@@ -5,9 +5,11 @@
 
 import requests
 
+from utils.comm_util import print_ts
+
 
 def translation(text):
-    print("发起百度翻译POST请求")
+    print_ts("发起百度翻译POST请求, 翻译文本: %s" % text)
     url = 'https://fanyi.baidu.com/sug'
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
@@ -21,15 +23,15 @@ def translation(text):
 
 
 def json_parse(dict_):
-    print("解析数据")
+    print_ts("解析数据, 翻译结果: ")
     json_data = {'errno': 0, 'data': [{'k': 'dog', 'v': 'n. 狗; 蹩脚货; 丑女人; 卑鄙小人 v. 困扰; 跟踪'}, {'k': 'DOG', 'v': 'abbr. Data Output Gate 数据输出门'}, {'k': 'doge', 'v': 'n. 共和国总督'},
                                       {'k': 'dogm', 'v': 'abbr. dogmatic 教条的; 独断的; dogmatism 教条主义; dogmatist'}, {'k': 'Dogo', 'v': '[地名] [马里、尼日尔、乍得] 多戈; [地名] [韩国] 道高'}]}
 
     # 获取到这一条数据: n. 狗; 蹩脚货; 丑女人; 卑鄙小人 v. 困扰; 跟踪
     data_ = dict_['data'][0]['v']
-    print(data_)
+    print_ts(data_)
 
 
 if __name__ == '__main__':
-    post_dict = translation("dog")
+    post_dict = translation("温文尔雅")
     json_parse(post_dict)
