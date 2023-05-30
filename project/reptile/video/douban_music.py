696bf0b79062f087e45d5666b9d4fb9eb81f8e31
MoncozGC <jadepengc@163.com>
2023-05-30 22:01:06 +0800

    feat: 爬取豆瓣音乐Top250数据优化, 进行分页查询

--- project/reptile/video/douban_music.py
+++ project/reptile/video/douban_music.py
@@ -2,7 +2,6 @@
 # Author: MoncozGC
 # Date  : 2023-05-29
 # Desc  : 爬取豆瓣音乐Top250数据, https://music.douban.com/top250
-import time
 
 import pandas as pd
 import requests
@@ -59,46 +58,53 @@ if __name__ == '__main__':
     url = "https://music.douban.com/top250"
     file_name = "data/douban_music.csv"
 
-    requests_get = requests.get(url=url, headers=headers)
-    # requests_get.encoding = 'gbk'
-    child_tree = etree.HTML(requests_get.text)
+    num = 0
+    for num_item in range(0, 10):
+        param = {
+            'start': num
+        }
+        requests_get = requests.get(url=url, headers=headers, params=param)
+        # requests_get.encoding = 'gbk'
+        child_tree = etree.HTML(requests_get.text)
 
-    dataset, reviews, reviews_info = [], '', ''
-    i = 0
-    for item in child_tree.xpath("//div[@class='pl2']"):
-        # 音乐名称
-        title = item.xpath("a/text()")[0]
-        # 详情链接, 获取评论
-        href = item.xpath("a/@href")[0]
-        # 详情信息
-        detail = item.xpath("p[@class='pl']/text()")[0]
-        # 歌手, 发行时间, 专辑类型, 介质, 音乐类别
-        details = detail.split(' / ')
-        # 评论人数
-        comments = item.xpath("div[@class='star clearfix']/span[@class='pl']/text()")[0]
+        dataset, reviews, reviews_info = [], '', ''
+        i = 0
+        for item in child_tree.xpath("//div[@class='pl2']"):
+            # 音乐名称
+            title = item.xpath("a/text()")[0]
+            # 详情链接, 获取评论
+            href = item.xpath("a/@href")[0]
+            # 详情信息
+            detail = item.xpath("p[@class='pl']/text()")[0]
+            # 歌手, 发行时间, 专辑类型, 介质, 音乐类别
+            details = detail.split(' / ')
+            # 评论人数
+            comments = item.xpath("div[@class='star clearfix']/span[@class='pl']/text()")[0]
 
-        # 访问子链接, 获取评论信息
-        child_get = requests.get(url=href, headers=headers)
-        child = etree.HTML(child_get.text)
+            # 访问子链接, 获取评论信息
+            child_get = requests.get(url=href, headers=headers)
+            child = etree.HTML(child_get.text)
 
-        # 获取评论信息
-        for child_item in child.xpath("//div[@class='review-list  ']"):
-            # 评论人员
-            reviews = child_item.xpath("//div/div[@id]/header[@class='main-hd']/a[@class='name']/text()")
-            # 评论具体信息
-            reviews_info = child_item.xpath("//div/div[@id]/div[@class='main-bd']/div[@id]/div[@class='short-content']/text()")
-            # 移除内容只有 ) 的数据
-            reviews_info = [item.strip() for item in reviews_info if ')' not in item]
+            # 获取评论信息
+            for child_item in child.xpath("//div[@class='review-list  ']"):
+                # 评论人员
+                reviews = child_item.xpath("//div/div[@id]/header[@class='main-hd']/a[@class='name']/text()")
+                # 评论具体信息
+                reviews_info = child_item.xpath("//div/div[@id]/div[@class='main-bd']/div[@id]/div[@class='short-content']/text()")
+                # 移除内容只有 ) 的数据
+                reviews_info = [item.strip() for item in reviews_info if ')' not in item]
 
-        dataset.append((title, details[0], details[1], details[4], reviews, reviews_info))
-        i += 1
-        # 控制查询的数据
-        # if i > 3:
-        #     break
-        print(title)
-        time.sleep(3)
+            dataset.append((title, details[0], details[1], details[4], reviews, reviews_info))
+            i += 1
+            # 控制查询的数据
+            # if i > 3:
+            #     break
+            print(title)
+            # time.sleep(3)
+        num += 25
+        print(num)
 
-    # 将dataset转化为df, 并以追加的模式写入csv文件中
-    for item in dataset:
-        result_df: DataFrame = df_transform(item)
-        result_df.to_csv(file_name, mode='a', header=False, index=False)
+        # 将dataset转化为df, 并以追加的模式写入csv文件中
+        for item in dataset:
+            result_df: DataFrame = df_transform(item)
+            result_df.to_csv(file_name, mode='a', header=False, index=False)
